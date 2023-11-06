cdadc8df55b98e2cc258c715d12d828c0b104109
mikey <mikey@hey.com>
2023-11-06 16:54:48 -0600

    add support for rendering images

---
+++ example-2-game.py
@@ -0,0 +1,37 @@
+# imports
+import pygame
+
+pygame.init()
+
+# configure screen
+screen = pygame.display.set_mode([500, 500])
+
+class GameObject(pygame.sprite.Sprite):
+    def __init__(self, x, y, image):
+        super(GameObject, self).__init__()
+        self.surf = pygame.image.load(image)
+        self.x = x
+        self.y = y
+        
+    def render(self, screen):
+        screen.blit(self.surf, (self.x, self.y))
+
+# instantiate GameObject
+apple = GameObject(120, 300, 'apple.png')
+strawberry = GameObject(0, 0, 'strawberry.png')
+
+running = True
+while running:
+    # look at events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    # clear screen
+    screen.fill((255, 255, 255))
+    
+    apple.render(screen)
+    strawberry.render(screen)
+    
+    # update display
+    pygame.display.flip()
